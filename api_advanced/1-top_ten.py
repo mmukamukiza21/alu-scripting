2aae13b8e120b61e230ddeae2ddcf6b9b918d3d5
mmukamukiza21 <m.mukamukiz@alustudent.com>
2025-11-22 14:01:55 +0000

    Fix PEP8 with proper line breaks

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -11,14 +11,11 @@ def top_ten(subreddit):
 
     url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
         .format(subreddit)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/91.0.4472.124 Safari/537.36'
-    }
+    headers = {'User-Agent': 'My User Agent 1.0'}
     
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
         if response.status_code == 200:
             data = response.json().get('data')
             if data:
