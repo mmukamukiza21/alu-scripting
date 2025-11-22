4df4b6d8667de9f8cec115ad8d4fa28b36700442
mmukamukiza21 <m.mukamukiz@alustudent.com>
2025-11-22 15:05:05 +0000

    Simplify to match original format exactly

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,17 +5,10 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the top 10 hot posts for a subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
-    params = {'limit': 10}
-    
-    response = requests.get(
-        url,
-        headers=headers,
-        params=params,
-        allow_redirects=False
-    )
-    
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json().get('data')
         children = data.get('children')
