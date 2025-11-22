ca4672c933a9a2b397d5c354a609d431bc196cf4
mmukamukiza21 <m.mukamukiz@alustudent.com>
2025-11-22 14:54:08 +0000

    Fix Reddit API to match expected output format

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,10 +5,13 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the top 10 hot posts for a subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
-        .format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = {'limit': 10}
+    
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    
     if response.status_code == 200:
         data = response.json().get('data')
         children = data.get('children')
